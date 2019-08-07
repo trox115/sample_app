7d29c1ec98dbf43c73b8935b5e5dd7ffa2313fc8
trox115 <tofernandes67@gmail.com>
2019-08-07 23:15:58 +0100

    migration solved

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_214058) do
+ActiveRecord::Schema.define(version: 2019_08_07_215020) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
