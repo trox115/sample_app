7ca610c5ddd7de8eece7a708f83f476bd6c86558
trox115 <tofernandes67@gmail.com>
2019-08-13 00:38:56 +0100

    errors with migrations solved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
