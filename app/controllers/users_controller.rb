a8b6a30c1c3269ace37e0d06805e28f874a92509
trox115 <tofernandes67@gmail.com>
2019-08-13 00:40:28 +0100

    added flashes message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
