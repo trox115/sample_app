4bfd3fd0861d775849c5e908dee7ed6e71e216ef
trox115 <tofernandes67@gmail.com>
2019-08-22 00:44:10 +0100

    corrected bugs of routes and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  resources :users
-  get 'users/new'
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
+  
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  delete '/logout',  to: 'sessions#destroy'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+ 
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  resources :users
   resources :account_activations, only: [:edit]
 
 
