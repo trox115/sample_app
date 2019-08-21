c9bc95845dfaff36149bc50271658ef232241934
trox115 <tofernandes67@gmail.com>
2019-08-21 21:43:03 +0100

    added faker, users with pagination , added admin to db

--- db/migrate/20190814225651_add_remember_digest_to_users.rb
+++ db/migrate/20190814225651_add_remember_digest_to_users.rb
@@ -1,5 +1,6 @@
 class AddRememberDigestToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :remember_digest, :string
+    add_column :users, :password_digest, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_225651) do
+ActiveRecord::Schema.define(version: 2019_08_21_204204) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_14_225651) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
